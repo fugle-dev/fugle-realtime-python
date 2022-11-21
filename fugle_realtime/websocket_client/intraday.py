8eca8563e238a9963fb0c3c183defe46f880fed7
Chun-Kai Wang <chunkai1312@gmail.com>
2022-11-21 10:21:48 +0800

    fix: missing error handler for websocket client

--- fugle_realtime/websocket_client/intraday.py
+++ fugle_realtime/websocket_client/intraday.py
@@ -6,17 +6,17 @@ class Intraday:
     def __init__(self, config):
         self.config = config
 
-    def meta(self, on_message=None, on_open=None, on_close=None, **params):
-        return self.createSocket(self.compile_url('/intraday/meta', params), on_message, on_open, on_close)
+    def meta(self, on_message=None, on_open=None, on_close=None, on_error=None, **params):
+        return self.createSocket(self.compile_url('/intraday/meta', params), on_message, on_open, on_close, on_error)
 
-    def quote(self, on_message=None, on_open=None, on_close=None, **params):
-        return self.createSocket(self.compile_url('/intraday/quote', params), on_message, on_open, on_close)
+    def quote(self, on_message=None, on_open=None, on_close=None, on_error=None, **params):
+        return self.createSocket(self.compile_url('/intraday/quote', params), on_message, on_open, on_close, on_error)
 
-    def chart(self, on_message=None, on_open=None, on_close=None, **params):
-        return self.createSocket(self.compile_url('/intraday/chart', params), on_message, on_open, on_close)
+    def chart(self, on_message=None, on_open=None, on_close=None, on_error=None, **params):
+        return self.createSocket(self.compile_url('/intraday/chart', params), on_message, on_open, on_close, on_error)
 
-    def createSocket(self, url, on_message, on_open, on_close):
-        return WSClient(url, on_message, on_open, on_close)
+    def createSocket(self, url, on_message, on_open, on_close, on_error):
+        return WSClient(url, on_message, on_open, on_close, on_error)
 
     def compile_url(self, path, params):
         params['apiToken'] = self.config['api_token']
